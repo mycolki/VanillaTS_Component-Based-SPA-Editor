54cdd2ac4f75adf9983e99334091fd90649c40a7
Colki <mycolki@gmail.com>
2023-03-23 02:56:55 +0900

    feat: 에디터방 레이아웃

--- src/pages/room.ts
+++ src/pages/room.ts
@@ -1,6 +1,20 @@
 function RoomPage(): HTMLElement {
   const container = document.createElement('div');
-  console.log('rom');
+  const editorWrapper = document.createElement('div');
+  const editor = document.createElement('textarea');
+  const sidebar = document.createElement('div');
+  const sidebarHeader = document.createElement('h1');
+  const userList = document.createElement('ul');
+  const message = document.createElement('p');
+
+  container.className = 'room-page__container';
+  editorWrapper.className = 'editor-wrapper';
+  sidebar.className = 'sidebar';
+  sidebarHeader.textContent = '참여자 목록';
+
+  sidebar.append(sidebarHeader, userList, message);
+  editorWrapper.append(editor);
+  container.append(editorWrapper, sidebar);
 
   return container;
 }
